342e60dd59b5416bb1b40f99e51f00cce49c4d6f
dMeenta2003 <demential2003@gmail.com>
2024-06-16 20:22:09 -0500

    pongan su ip local

--- app/src/main/java/dev/didnt/proyecto/servicio/WebService.kt
+++ app/src/main/java/dev/didnt/proyecto/servicio/WebService.kt
@@ -11,7 +11,7 @@ import retrofit2.http.POST
 import retrofit2.http.Path
 
 object AppConstantes{
-    const val BASE_URL = "http://192.168.56.1" //Añadir ip
+    const val BASE_URL = "http://:3000" //Añadir ip
 }
 
 interface WebService {
