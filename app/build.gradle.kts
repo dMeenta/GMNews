8675a10636304355fe69945a0ee1302460b741cf
dMeenta2003 <demential2003@gmail.com>
2024-06-21 19:22:12 -0500

    agregado Glide

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,9 +48,10 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
     implementation("com.google.code.gson:gson:2.10.1")
     implementation("com.squareup.retrofit2:converter-gson:2.11.0")
-
     //Coroutines
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
+    //Glide
+    implementation ("com.github.bumptech.glide:glide:4.16.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
