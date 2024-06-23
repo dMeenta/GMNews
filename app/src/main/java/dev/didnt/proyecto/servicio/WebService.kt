854c7f03a1872a2a7d6d85f673978e51a9978b19
dMeenta2003 <demential2003@gmail.com>
2024-06-23 15:25:37 -0500

    se mueve entre ventanas Profile, Home y Exit

--- app/src/main/java/dev/didnt/proyecto/servicio/WebService.kt
+++ app/src/main/java/dev/didnt/proyecto/servicio/WebService.kt
@@ -7,8 +7,8 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.PATCH
 import retrofit2.http.POST
-import retrofit2.http.PUT
 import retrofit2.http.Path
 
 object AppConstantes{
