6f13807ff54e97ac320eed4f894206c3e7237f5d
dMeenta2003 <demential2003@gmail.com>
2024-07-04 19:34:33 -0500

    aaa

---
+++ app/src/main/java/dev/didnt/proyecto/ui/ProfileFragment.kt
@@ -0,0 +1,99 @@
+package dev.didnt.proyecto.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.LinearLayout
+import android.widget.TextView
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import dev.didnt.proyecto.R
+import dev.didnt.proyecto.entidad.Usuario
+import dev.didnt.proyecto.servicio.RetrofitClient
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class ProfileFragment : Fragment() {
+    private lateinit var txtModifyName: EditText
+    private lateinit var txtModifyEmail: EditText
+    private lateinit var btnEdit: ImageButton
+    private lateinit var btnGuardar: Button
+    private lateinit var lblUserName: TextView
+    private lateinit var lblIdOnline: TextView
+    private lateinit var lblUserEdad: TextView
+    private lateinit var lblUserGenero: TextView
+    private lateinit var lblUserEmail: TextView
+    private lateinit var editForm: LinearLayout
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_profile, container, false)
+        var userId = arguments?.getInt("id", 0)
+        var userIdOnline = arguments?.getString("userIdOnline")
+        var userPassword = arguments?.getString("userPassword")
+        val userName = arguments?.getString("userName")
+        var userEmail = arguments?.getString("userEmail")
+        var userEdad = arguments?.getInt("userEdad", 0)
+        var userGenero = arguments?.getString("userGenero")
+        var gender:String
+        if(userGenero=="M"){
+            gender = "Masculino"
+        }else{
+            gender = "Femenino"
+        }
+
+        lblUserName = view.findViewById(R.id.lblNombreUser)
+        lblIdOnline = view.findViewById(R.id.lblIdOnline)
+        lblUserEdad = view.findViewById(R.id.lblEdadUser)
+        lblUserGenero = view.findViewById(R.id.lblGeneroUser)
+        lblUserEmail = view.findViewById(R.id.lblCorreoUser)
+        editForm = view.findViewById(R.id.editForm)
+
+        lblUserName.text = userName
+        lblIdOnline.text = "idOnline: "+userIdOnline
+        lblUserEdad.text = "Edad: "+userEdad.toString()
+        lblUserGenero.text = "Genero: "+ gender
+        lblUserEmail.text = userEmail
+
+        btnGuardar = view.findViewById(R.id.btnGuardar)
+        txtModifyName = view.findViewById(R.id.txtModifyName)
+        txtModifyEmail = view.findViewById(R.id.txtModifyEmail)
+        btnEdit = view.findViewById(R.id.btnEdit)
+        btnEdit.setOnClickListener{
+            editForm.visibility = View.VISIBLE
+
+            txtModifyName.setText(userName)
+            txtModifyEmail.setText(userEmail)
+
+        }
+        btnGuardar.setOnClickListener {
+            editForm.visibility = View.GONE
+            actualizar(userId)
+        }
+
+        return view
+    }
+    private fun actualizar(id:Int?){
+        val nombre = txtModifyName.text.toString().trim()
+        val email = txtModifyEmail.text.toString().trim()
+
+        val user = Usuario(0, "", "", nombre, email, 0, "")
+
+        CoroutineScope(Dispatchers.IO).launch {
+            val res = RetrofitClient.webService.modificarUsuario(id, user)
+            requireActivity().runOnUiThread {
+                if(res.isSuccessful){
+                    Toast.makeText(context, res.body().toString(), Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+}
