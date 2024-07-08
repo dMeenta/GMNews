7d2cf52b098108d16a4874d4a4cba9f42f7ee1c0
dMeenta2003 <demential2003@gmail.com>
2024-07-07 20:28:09 -0500

    arreglo del perfil y del exit

--- app/src/main/java/dev/didnt/proyecto/ui/ExitFragment.kt
+++ app/src/main/java/dev/didnt/proyecto/ui/ExitFragment.kt
@@ -10,6 +10,7 @@ import android.widget.Button
 import android.widget.TextView
 import androidx.core.app.TaskStackBuilder
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 import dev.didnt.proyecto.IngresarActivity
 import dev.didnt.proyecto.R
 
@@ -23,12 +24,25 @@ class ExitFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_exit, container, false)
-        val userName = arguments?.getString("userName")
+
+        val fbUser = FirebaseAuth.getInstance().currentUser
+        val usersRef = FirebaseDatabase.getInstance().getReference("Usuarios")
+        val currentUser = usersRef.child(fbUser!!.uid)
+
+        currentUser.get().addOnSuccessListener { dataSnapshot ->
+            if (dataSnapshot.exists()) {
+                val userData = dataSnapshot.getValue() as Map<*, *>
+
+                val nombre = userData["nombre"] as String
+
+                lblUserName.text = nombre + " :( ?"
+            }
+        }
 
         btnExit = view.findViewById(R.id.btnCerrar)
         lblUserName = view.findViewById(R.id.lblUserName)
 
-        lblUserName.text = userName + " :( ?"
+
         btnExit.setOnClickListener {
             FirebaseAuth.getInstance().signOut()
             val activity = requireActivity()
--- app/src/main/java/dev/didnt/proyecto/ui/ProfileFragment.kt
+++ app/src/main/java/dev/didnt/proyecto/ui/ProfileFragment.kt
@@ -1,5 +1,7 @@
 package dev.didnt.proyecto.ui
 
+import android.annotation.SuppressLint
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -10,82 +12,133 @@ import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.LinearLayout
 import android.widget.TextView
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import dev.didnt.proyecto.R
+import dev.didnt.proyecto.entidad.Usuario
 
 class ProfileFragment : Fragment() {
 
+    private lateinit var profileActivity:LinearLayout
     private lateinit var lblUserName: TextView
     private lateinit var lblIdOnline: TextView
     private lateinit var lblUserEdad: TextView
     private lateinit var lblUserGenero: TextView
     private lateinit var lblUserEmail: TextView
+    private lateinit var lblFechaNacimiento: TextView
     private lateinit var btnEdit: ImageButton
 
     private lateinit var editForm:LinearLayout
-    private lateinit var txtModifyName: EditText
-    private lateinit var txtModifyEmail: EditText
+    private lateinit var txt_edit_name: EditText
+    private lateinit var txt_edit_idOnline: EditText
     private lateinit var btnGuardar: Button
+    private lateinit var btnCancelarEdit: Button
+    private lateinit var profileInfo: LinearLayout
 
+    @SuppressLint("MissingInflatedId")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_profile, container, false)
+
         val fbUser = FirebaseAuth.getInstance().currentUser
         val usersRef = FirebaseDatabase.getInstance().getReference("Usuarios")
         val currentUser = usersRef.child(fbUser!!.uid)
+
+        profileActivity = view.findViewById(R.id.profileActivity)
         lblUserName = view.findViewById(R.id.lblNombreUser)
         lblIdOnline = view.findViewById(R.id.lblIdOnline)
         lblUserEdad = view.findViewById(R.id.lblEdadUser)
         lblUserGenero = view.findViewById(R.id.lblGeneroUser)
         lblUserEmail = view.findViewById(R.id.lblCorreoUser)
+        lblFechaNacimiento = view.findViewById(R.id.lblFechaUser)
 
-        /*lblUserName.text = fbUser.uid
-        lblIdOnline.text = "idOnline: "+userIdOnline
-        lblUserEdad.text = "Edad: "+userEdad.toString()
-        lblUserGenero.text = "Género: "+ gender
-        lblUserEmail.text = userEmail*/
+        currentUser.get().addOnSuccessListener {dataSnapshot->
+            if(dataSnapshot.exists()){
+                val userData = dataSnapshot.getValue() as Map<*,*>
 
+                val nombre = userData["nombre"] as String
+                val idOnline = userData["idOnline"] as String
+                val genero = userData["genero"] as String
+                val fechaNacimiento = userData["fechaNacimiento"] as String
+                val email = userData["email"] as String
+                val edad = userData["edad"] as Any
+
+                //EditForm
+                txt_edit_idOnline.setText(idOnline)
+                txt_edit_name.setText(nombre)
+
+                //UserInfo
+                lblUserName.text = nombre
+                lblIdOnline.text = "idOnline: "+idOnline
+                lblUserEdad.text = "Edad: "+edad.toString()
+                lblUserGenero.text = "Género: "+ genero
+                lblUserEmail.text = email
+                lblFechaNacimiento.text = "Fecha de nacimiento: "+fechaNacimiento
+            }
+        }
         editForm = view.findViewById(R.id.editForm)
+        profileInfo = view.findViewById(R.id.profile_info)
         btnGuardar = view.findViewById(R.id.btnGuardar)
-        txtModifyName = view.findViewById(R.id.txtModifyName)
-        txtModifyEmail = view.findViewById(R.id.txtModifyEmail)
+        btnCancelarEdit = view.findViewById(R.id.btnCancelarEdit)
+        txt_edit_idOnline = view.findViewById(R.id.txt_edit_idOnline)
+        txt_edit_name = view.findViewById(R.id.txt_edit_name)
         btnEdit = view.findViewById(R.id.btnEdit)
-
         btnEdit.setOnClickListener{
+            val edit_bg: Drawable? = context?.getDrawable(R.drawable.edit_form_bg)
+            profileActivity.background = edit_bg
+            profileInfo.visibility = View.GONE
             editForm.visibility = View.VISIBLE
-
-            /*txtModifyName.setText(userName)
-            txtModifyEmail.setText(userEmail)*/
         }
-        btnGuardar.setOnClickListener {
+        btnCancelarEdit.setOnClickListener {
+            val normal_bg: Drawable? = context?.getDrawable(R.drawable.profile_bg)
+            profileActivity.background = normal_bg
+            profileInfo.visibility = View.VISIBLE
             editForm.visibility = View.GONE
-            /*actualizar(userId)*/
         }
+
+        btnGuardar.setOnClickListener {
+            val normal_bg: Drawable? = context?.getDrawable(R.drawable.profile_bg)
+            profileActivity.background = normal_bg
+            profileInfo.visibility = View.VISIBLE
+            editForm.visibility = View.GONE
+
+            val updatedData = mapOf(
+                "idOnline" to txt_edit_idOnline.text.toString().trim(),
+                "nombre" to txt_edit_name.text.toString().trim()
+            )
+
+            currentUser.updateChildren(updatedData).addOnSuccessListener {
+                Toast.makeText(requireContext(), "Usuario actualizado correctamente", Toast.LENGTH_SHORT).show()
+            }.addOnFailureListener {e->
+                Toast.makeText(requireContext(), "Error al actualizar los datos \n ${e.message}", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        currentUser.addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if (snapshot.exists()) {
+                    val userData = snapshot.getValue(Usuario::class.java)
+                    if (userData != null) {
+                        actualizarUI(userData)
+                    }
+                }
+            }
+            override fun onCancelled(error: DatabaseError) {
+            }
+
+        })
 
         return view
     }
-
-
-    private fun actualizar(id:Int?){
-
-        val nombre = txtModifyName.text.toString().trim()
-        val email = txtModifyEmail.text.toString().trim()
-
-        /*val user = Usuario(0, "", "", nombre, email, 0, "")
-
-        CoroutineScope(Dispatchers.IO).launch {
-            val rpta = id?.let { RetrofitClient.webService.modificarUsuario(it, user) }
-                requireActivity().runOnUiThread {
-                    if (rpta != null) {
-                        if(rpta.isSuccessful){
-                            Toast.makeText(context, rpta.body().toString(), Toast.LENGTH_SHORT).show()
-                        }
-                    }
-            }
-        }*/
+    private fun actualizarUI(usuario: Usuario){
+        lblIdOnline.text = usuario.idOnline
+        lblUserName.text = usuario.nombre
     }
 }
